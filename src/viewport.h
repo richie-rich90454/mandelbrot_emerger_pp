db78c93d2fac309d4b6cd5ac0dc9ea13b09cb49a
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 10:10:47 +0800

    Extend Viewport with autopilot zoom operations

--- src/viewport.h
+++ src/viewport.h
@@ -7,10 +7,17 @@ public:
     Viewport(int cssWidth, int cssHeight);
     void beginZoom(double deviceX, double deviceY);
     void completeZoom(double secondDeviceX, double secondDeviceY);
+    void autoZoom(double centerX, double centerY, double divisor);
+    void resetToInitial();
+    double getXi() const;
+    double getXf() const;
+    double getYi() const;
+    double getYf() const;
     double planeFromDeviceX(double deviceX) const;
     double planeFromDeviceY(double deviceY) const;
     void log(std::ostream& stream) const;
 private:
+    void initializeBounds();
     int cssWidth;
     int cssHeight;
     double aspectRatio;
