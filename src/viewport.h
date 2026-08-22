25f58f359de2b21aca03d038de0b199d3ff0461f
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 09:48:45 +0800

    Add Viewport class

---
+++ src/viewport.h
@@ -0,0 +1,25 @@
+#ifndef VIEWPORT_H
+#define VIEWPORT_H
+#include <iosfwd>
+const int RES=2;
+class Viewport{
+public:
+    Viewport(int cssWidth, int cssHeight);
+    void beginZoom(double deviceX, double deviceY);
+    void completeZoom(double secondDeviceX, double secondDeviceY);
+    double planeFromDeviceX(double deviceX) const;
+    double planeFromDeviceY(double deviceY) const;
+    void log(std::ostream& stream) const;
+private:
+    int cssWidth;
+    int cssHeight;
+    double aspectRatio;
+    double boundXi;
+    double boundXf;
+    double boundYi;
+    double boundYf;
+    bool selectionPending;
+    double pendingPlaneX;
+    double pendingPlaneY;
+};
+#endif
