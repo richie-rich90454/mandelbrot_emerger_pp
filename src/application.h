07bec0ba1a6ae737bce932d9e67c8ec61fb4934f
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 09:48:53 +0800

    Add Application class

---
+++ src/application.h
@@ -0,0 +1,44 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+#include "viewport.h"
+#include "simulation.h"
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+union SDL_Event;
+class ColorScheme;
+class Application{
+public:
+    Application(int cssWidth, int cssHeight);
+    ~Application();
+    bool initialize();
+    void run();
+private:
+    void processEvents();
+    void render();
+    void onMouseButtonDown(const SDL_Event& event);
+    void onKeyDown(const SDL_Event& event);
+    void cycleColorScheme();
+    void toggleFullscreen();
+    void saveScreenshot();
+    void computeDestinationRect();
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    Viewport viewport;
+    Simulation simulation;
+    ColorScheme* schemes[3];
+    int activeScheme;
+    bool clicker;
+    bool running;
+    bool fullscreen;
+    unsigned long long startTicks;
+    int windowWidth;
+    int windowHeight;
+    float dstX;
+    float dstY;
+    float dstW;
+    float dstH;
+    float scale;
+};
+#endif
