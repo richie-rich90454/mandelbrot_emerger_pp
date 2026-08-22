835645b01aa2592eac6d9cefd7554418f9a401bc
richie-rich90454 <rjiang880@gmail.com>
2026-08-22 12:10:11 +0800

    Add rainbow, fire and ice color schemes

--- src/color_scheme.h
+++ src/color_scheme.h
@@ -11,6 +11,7 @@ public:
     ColorScheme();
     virtual ~ColorScheme();
     virtual void shade(double brightness, Rgba& out) const=0;
+    virtual const char* name() const=0;
 protected:
     static unsigned char clampChannel(double value);
 };
@@ -19,17 +20,41 @@ public:
     GrayscaleScheme();
     virtual ~GrayscaleScheme();
     virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
 };
 class ThermalScheme:public ColorScheme{
 public:
     ThermalScheme();
     virtual ~ThermalScheme();
     virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
 };
 class AlphaScheme:public ColorScheme{
 public:
     AlphaScheme();
     virtual ~AlphaScheme();
     virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
+};
+class RainbowScheme:public ColorScheme{
+public:
+    RainbowScheme();
+    virtual ~RainbowScheme();
+    virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
+};
+class FireScheme:public ColorScheme{
+public:
+    FireScheme();
+    virtual ~FireScheme();
+    virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
+};
+class IceScheme:public ColorScheme{
+public:
+    IceScheme();
+    virtual ~IceScheme();
+    virtual void shade(double brightness, Rgba& out) const override;
+    virtual const char* name() const override;
 };
 #endif
