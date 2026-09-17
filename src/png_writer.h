f2cadbfb9c9988ef94d7bf9055156fcd1f12052c
richie-rich90454 <rjiang880@gmail.com>
2026-09-17 16:54:18 +0800

    Accept raw pixels and dimensions in the PNG writer interface

--- src/png_writer.h
+++ src/png_writer.h
@@ -4,16 +4,15 @@
 #include <fstream>
 #include <string>
 #include <vector>
-class PixelBuffer;
 class PngWriter{
 public:
     PngWriter();
     ~PngWriter();
-    bool save(const PixelBuffer& buffer, const std::string& path) const;
+    bool save(const unsigned char* pixels, int width, int height, const std::string& path) const;
 private:
     void writeChunk(std::ofstream& stream, const char* type, const std::vector<unsigned char>& data) const;
     static void pushU32(std::vector<unsigned char>& out, unsigned int value);
-    static unsigned int crc32(const unsigned char* data, std::size_t length);
+    static unsigned int crc32Update(unsigned int crc, const unsigned char* data, std::size_t length);
     static unsigned int adler32(const unsigned char* data, std::size_t length);
 };
 #endif
