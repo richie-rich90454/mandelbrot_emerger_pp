936435c256e2544e028c8c7c0d58277130d7962d
richie-rich90454 <rjiang880@gmail.com>
2026-09-19 14:08:36 +0800

    Drive the tone lookup table from relative escape counts

--- src/simulation.h
+++ src/simulation.h
@@ -7,14 +7,18 @@ class Simulation{
 public:
     Simulation(int deviceWidth, int deviceHeight);
     void reframe(const Viewport& viewport);
-    void step(int passCount, const ColorScheme* scheme, unsigned char* pixels, int pitch, double nowSeconds);
+    void step(int passCount, const ColorScheme* scheme, unsigned char* pixels, int pitch);
+    long long passesSinceReframe() const;
 private:
-    void renderRows(int startRow, int endRow, int passCount, double nowSeconds, double toneOffset, const ColorScheme* scheme, unsigned char* pixels, int pitch);
+    void renderRows(int startRow, int endRow, int passCount, long long baseIndex, double toneScale, const ColorScheme* scheme, unsigned char* pixels, int pitch);
     int width;
     int height;
     std::vector<double> z;
     std::vector<double> diverge;
     std::vector<double> planeX;
     std::vector<double> planeY;
+    std::vector<double> toneTable;
+    long long frameIndex;
+    long long reframeIndex;
 };
 #endif
